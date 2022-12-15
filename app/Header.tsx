66ba70d1f9ea563c950143e77162f8defc2f3d11
Farrid <farridguntoro@gmail.com>
2022-12-16 02:06:33 +0700

    Co-authored-by: farridkun-test <farridkun-test@users.noreply.github.com>

---
+++ app/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Bars3Icon } from '@heroicons/react/24/solid/react'
+
+function Header() {
+  return (
+    <header>
+        <div>
+            <Bars3Icon />
+        </div>
+    </header>
+  )
+}
+
+export default Header
